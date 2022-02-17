b59acf7f47e0a5cde805ce820f326b43cfbea4aa
That Blonde Kid <72944557+PheonixMMKC777@users.noreply.github.com>
2022-02-17 13:07:03 -0600

    Add files via upload

---
+++ Win11_TPM_CPU_Skip.ps1
@@ -0,0 +1,6 @@
+﻿
+# Win 11 TPM/CPU Skip
+
+$Path = "Registry::HKEY_LOCAL_MACHINE\SYSTEM\Setup\MoSetup"
+New-ItemProperty -Path $Path -Name "AllowUpgradesWithUnsupportedTPMOrCPU" -Value "1" -PropertyType DWord
+
